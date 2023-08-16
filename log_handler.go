338df8f4303825035ebb71efdcb4f0642f7df4ce
miaoyin <miaoyin@woda.com>
2023-08-16 13:13:16 +0800

    feat: log line num

--- log_handler.go
+++ log_handler.go
@@ -1,11 +1,14 @@
 package kgin
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/xuender/kit/logs"
 )
+
+const _calldepth = 4
 
 func LogHandler(ctx *gin.Context) {
 	start := time.Now()
@@ -17,12 +20,12 @@ func LogHandler(ctx *gin.Context) {
 		query = "?" + query
 	}
 
-	logs.I.Printf("[%d] %v %s (%s) %s%s",
+	_ = logs.I.Output(_calldepth, fmt.Sprintf("[%d] %v %s (%s) %s%s",
 		ctx.Writer.Status(),
 		time.Since(start),
 		ctx.ClientIP(),
 		ctx.Request.Method,
 		ctx.Request.URL.Path,
 		query,
-	)
+	))
 }
